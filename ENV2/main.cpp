b758675904d5fd3fc6181b3cb9ecf6ff3ab78224
Narek <137328861+NarMadat@users.noreply.github.com>
2024-09-22 20:07:19 +0400

    Using sort.cpp in main.cpp

--- ENV2/main.cpp
+++ ENV2/main.cpp
@@ -6,6 +6,8 @@ int main() {
 	Complex numbers[] = {Complex(3,4), Complex(1,2)};
 	int size = sizeof(numbers) / sizeof(numbers[0]);
 
+	sort(numbers, size);
+
 	for (int i = 0; i < size; i++){
 		std::cout << numbers[i] << std::endl;
 	}
