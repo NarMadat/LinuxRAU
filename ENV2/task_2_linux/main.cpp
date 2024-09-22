a4aa1e3c6ec426a57b2370c04570648f19340c33
Narek <137328861+NarMadat@users.noreply.github.com>
2024-09-22 11:43:23 +0400

    Add files via upload

---
+++ ENV2/task_2_linux/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "complex.h"
+#include "sort.h"
+
+int main() {
+	Complex numbers[] = {Complex(3,4), Complex(1,2)};
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+
+	for (int i = 0; i < size; i++){
+		std::cout << numbers[i] << std::endl;
+	}
+
+	return 0;
+}
+
+
---
+++ ENV2/task_2_linux/sort.h
@@ -0,0 +1,5 @@
+#include "complex.h"
+
+void sort(Complex arr[], int size);
+
+
