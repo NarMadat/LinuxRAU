d3d9c1182244a56518665c1c0243f43a7dfe4e5b
Narek <narmadatian@gmail.com>
2024-11-04 00:03:39 +0400

    Adding task FS-4

---
+++ FS-4/redirect-cin.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <string>
+#include <cstdlib>
+
+void initialize (int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr<<"Error"<<std::endl;
+		exit(1);
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		std::cerr<<"Error"<<std::endl;
+		exit(2);
+	}
+	
+	if (dup2(fd, STDIN_FILENO) == -1)
+	{
+		std::cerr<<"Error"<<std::endl;
+		close (fd);
+		exit(3);
+	}
+	close(fd);
+}
+
+int main (int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr<<"Errror"<<std::endl;
+		return 1;
+	}
+	initialize(argc, argv);
+
+	std::string input;
+	std::cin >> input;
+
+	std::string reversed(input.rbegin(), input.rend());
+
+	std::cout<<reversed<<std::endl;
+	return 0;
+
+}
